5dfad2b894c1803118b85d12ff97a668abd0d9e8
kuramagod <156893596+kuramagod@users.noreply.github.com>
2026-04-01 13:43:24 +0300

    Отображение товаров и категорий из базы данных в каталог

--- core/views.py
+++ core/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product, Category
 
 
 def home_page(request):
     return render(request, "core/index.html")
 
 def catalog_page(request):
-    return render(request, "core/catalog.html")
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    return render(request, "core/catalog.html", {"products": products, "categories": categories})
 
 def about_page(request):
     return render(request, "core/about.html")
