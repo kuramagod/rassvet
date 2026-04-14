aebc99a358a76d49f83d3d1dd487772ccb6541a3
kuramagod <156893596+kuramagod@users.noreply.github.com>
2026-04-14 16:08:46 +0300

    Добавления параметров запуска проекта.

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+set -o errexit
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+
+python manage.py loaddata categories.json || true
+python manage.py loaddata products.json || true
+python manage.py loaddata characteristics.json || true
+python manage.py loaddata news.json || true
+
+python manage.py collectstatic --noinput
